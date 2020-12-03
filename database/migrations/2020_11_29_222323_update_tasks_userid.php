e0df4383980133a3cb02bd4714cb28ed6bb2bd89
yuuki sanada <poteti401@gmail.com>
2020-12-03 13:28:38 +0000

    kadai15-1 2

--- database/migrations/2020_11_29_222323_update_tasks_userid.php
+++ database/migrations/2020_11_29_222323_update_tasks_userid.php
@@ -16,7 +16,7 @@ class UpdateTasksUserid extends Migration
         Schema::table('tasks', function (Blueprint $table) {
             $table->unsignedBigInteger('user_id');
             
-            //$table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
